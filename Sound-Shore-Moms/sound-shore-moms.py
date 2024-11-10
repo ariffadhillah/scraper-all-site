05226e92d34d89f3640f7105b46a8181c0948b13
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-10 19:38:38 +0700

    new-york site Sound Shore Moms  section Guide - Where to go Strawberry Picking in and around Westchester

--- Sound-Shore-Moms/sound-shore-moms.py
+++ Sound-Shore-Moms/sound-shore-moms.py
@@ -55,7 +55,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
 }
 
-response = requests.get('https://soundshoremoms.com/get-lost-in-fun-the-best-corn-mazes-in-and-around-westchester-ny/',  cookies=cookies, headers=headers)
+response = requests.get('https://soundshoremoms.com/where-to-go-strawberry-picking-near-westchester/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -394,7 +394,7 @@ results = []
 
 
 
-title_name = 'Guides - Get Lost in Fun: The Best Corn Mazes in and Around Westchester, NY'
+title_name = 'Guides - Where to go Strawberry Picking in and around Westchester'
 
 reviews_1 = soup.find_all('div', {'class': 'et_pb_text_inner'})
 
