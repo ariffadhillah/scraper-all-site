d599d6dabdded6e4ed90c1bbe00d0dbab306924f
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-12 15:48:09 +0700

    new-york site hamptonsmoms section Personal Chefs

--- The-Hamptons/the-hamptons.py
+++ The-Hamptons/the-hamptons.py
@@ -395,13 +395,13 @@ headers = {
 import re
 
 
-response = requests.get('https://hamptonsmoms.com/resources/real-estate-agents/',  cookies=cookies, headers=headers)
+response = requests.get('https://hamptonsmoms.com/resources/personal-chefs/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'Real Estate Agents'
+title_name = 'Personal Chefs'
 
 page_section = soup.find_all('div', {'class': 'et_pb_toggle_content clearfix'})
 
