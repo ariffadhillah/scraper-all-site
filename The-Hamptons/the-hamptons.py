750dfbf9df8c7fc2124bcad21049b501cb98d9f0
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-12 15:48:49 +0700

    new-york site hamptonsmoms section Places to Eat

--- The-Hamptons/the-hamptons.py
+++ The-Hamptons/the-hamptons.py
@@ -395,13 +395,13 @@ headers = {
 import re
 
 
-response = requests.get('https://hamptonsmoms.com/resources/personal-chefs/',  cookies=cookies, headers=headers)
+response = requests.get('https://hamptonsmoms.com/resources/places-to-eat/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'Personal Chefs'
+title_name = 'Places to Eat'
 
 page_section = soup.find_all('div', {'class': 'et_pb_toggle_content clearfix'})
 
