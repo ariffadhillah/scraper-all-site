d46ffe324fe7f6615b06f81cb50bd1b8cddceae7
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-12 11:20:42 +0700

    new-york site hamptonsmoms section Dentist

--- The-Hamptons/the-hamptons.py
+++ The-Hamptons/the-hamptons.py
@@ -395,13 +395,13 @@ headers = {
 import re
 
 
-response = requests.get('https://hamptonsmoms.com/resources/day-camps/',  cookies=cookies, headers=headers)
+response = requests.get('https://hamptonsmoms.com/resources/dentists/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'Day Camps'
+title_name = 'Dentist'
 
 page_section = soup.find_all('div', {'class': 'et_pb_toggle_content clearfix'})
 
