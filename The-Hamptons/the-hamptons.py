3520533f695b6d8fca332ac7f76e8dbd4c49e81f
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-12 11:21:55 +0700

    new-york site hamptonsmoms section Doctor - Urgent Care

--- The-Hamptons/the-hamptons.py
+++ The-Hamptons/the-hamptons.py
@@ -395,13 +395,13 @@ headers = {
 import re
 
 
-response = requests.get('https://hamptonsmoms.com/resources/dentists/',  cookies=cookies, headers=headers)
+response = requests.get('https://hamptonsmoms.com/resources/doctors-urgent-care/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'Dentist'
+title_name = 'Doctor - Urgent Care'
 
 page_section = soup.find_all('div', {'class': 'et_pb_toggle_content clearfix'})
 
