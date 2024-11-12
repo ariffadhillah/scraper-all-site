d874fac688b2e4637d9e829f328b068b71a43342
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-12 15:51:48 +0700

    new-york site hamptonsmoms section Working Spaces site finihsed

--- The-Hamptons/the-hamptons.py
+++ The-Hamptons/the-hamptons.py
@@ -395,13 +395,13 @@ headers = {
 import re
 
 
-response = requests.get('https://hamptonsmoms.com/resources/ways-to-get-around/',  cookies=cookies, headers=headers)
+response = requests.get('https://hamptonsmoms.com/resources/working-spaces/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'Ways to Get Around'
+title_name = 'Working Spaces'
 
 page_section = soup.find_all('div', {'class': 'et_pb_toggle_content clearfix'})
 
