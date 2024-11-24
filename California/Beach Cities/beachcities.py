b983c5b1cc349403bcb88dc841f2e00f7c777f7b
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-24 21:50:00 +0700

    California site beachcitiesmoms.com section Childrens Shopping

--- California/Beach Cities/beachcities.py
+++ California/Beach Cities/beachcities.py
@@ -29,22 +29,22 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 }
 
-response = requests.get('https://beachcitiesmoms.com/schools/redondo-beach/',  cookies=cookies, headers=headers)
+response = requests.get('https://beachcitiesmoms.com/resources/childrens-shopping/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'Schools - Redondo Beach'
+title_name = 'Childrens Shopping'
 
-reviews_1 = soup.find_all('div', {'class': 'et_pb_tab_content'})
+reviews_1 = soup.find_all('div', {'class': 'entry-content'})
 
 for page_ in reviews_1:
-    paragraphs_1 = page_.find_all('p')
+    paragraphs_1 = page_.find_all('div')
     # print(paragraphs_1)
 
     for p in paragraphs_1:
-        name_element = p.find('strong')
+        name_element = p.find('a')
         name = name_element.text.strip() if name_element else ''
         a_tag = p.find('a')
         if a_tag:
