fff312d60f801e4b17371c3c6958b2eec6c685d6
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-24 21:11:38 +0700

    California site beachcitiesmoms.com section Libraries

--- California/Beach Cities/beachcities.py
+++ California/Beach Cities/beachcities.py
@@ -29,13 +29,13 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 }
 
-response = requests.get('https://beachcitiesmoms.com/resources/new-moms/',  cookies=cookies, headers=headers)
+response = requests.get('https://beachcitiesmoms.com/resources/attractions/libraries/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'New Moms'
+title_name = 'Attractions - Libraries'
 
 reviews_1 = soup.find_all('div', {'class': 'entry-content'})
 
