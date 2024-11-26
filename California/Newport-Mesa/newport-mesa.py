1f38321dd834a29be65ab2d2fafddb6e0b43d522
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-11-26 22:15:05 +0700

    California site newportmesamoms.com section pet care

--- California/Newport-Mesa/newport-mesa.py
+++ California/Newport-Mesa/newport-mesa.py
@@ -29,13 +29,13 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 }
 
-response = requests.get('https://newportmesamoms.com/resources/new-mom/',  cookies=cookies, headers=headers)
+response = requests.get('https://newportmesamoms.com/resources/real-estate/',  cookies=cookies, headers=headers)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 results = []
 
-title_name = 'New Moms'
+title_name = 'Real Estate'
 
 reviews_1 = soup.find_all('div', {'class': 'et_pb_text_inner'})
 
