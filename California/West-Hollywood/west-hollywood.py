c9cb3ce19f3a9a5bd43937cc5417a79cc607f715
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-12-01 21:34:27 +0700

    scrape site thewesthollywoodmoms.com  county West Hollywood section Reading

--- California/West-Hollywood/west-hollywood.py
+++ California/West-Hollywood/west-hollywood.py
@@ -71,7 +71,7 @@ def open_to_website(browser):
     results = []
 
 
-    browser.get("https://thewesthollywoodmoms.com/resources/mom-life/")
+    browser.get("https://thewesthollywoodmoms.com/resources/reading/")
     time.sleep(10)
 
 
@@ -81,7 +81,7 @@ def open_to_website(browser):
     soup = BeautifulSoup(page_source, "html.parser")
 
 
-    output_ = 'Mom Life'
+    output_ = 'Reading'
 
     divs = soup.find_all('div', class_='et_pb_toggle_content clearfix')
 
