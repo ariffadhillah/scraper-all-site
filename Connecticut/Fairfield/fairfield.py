38a234e1586ab00527ceee15545c9722641315e7
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-12-07 15:20:36 +0700

    scrape site fairfieldctmoms.com country Connecticut county Fairfield section Childcare

--- Connecticut/Fairfield/fairfield.py
+++ Connecticut/Fairfield/fairfield.py
@@ -41,7 +41,7 @@ def setup_browser():
     browser.maximize_window()
 
     # Buka halaman awal (wajib sebelum menambahkan cookie)
-    browser.get("https://thewesthollywoodmoms.com/")
+    browser.get("https://fairfieldctmoms.com/")
     time.sleep(5)  # Tunggu hingga halaman terbuka
 
 
@@ -50,10 +50,10 @@ def setup_browser():
         {"name": "_fbp", "value": "fb.1.1733445962427.912197316380830365"},
         {"name": "_ga", "value": "GA1.1.804256592.1733445962"},
         {"name": "cf_clearance", "value": "g2FDNviaoha8YVdYKt43w3OA.nacCcQydfjmOV2ZwYo-1727279461-1.2.1.1-h0PXEtZMtBkncTrthRi9uKLoO7bxO_t98Z7dui0Nn01Ny7VTXZenvCRmtFzjwXoila8et8LOjwVLd_OinW5YpNm0zZ73Ogp8Mivg_EKPPYvi5t2i2c5SJvaeYNkQmsbOzT6rtcoIDAA3YNzLXwQHiP9eQRK.S2ti4wowp.5nApEz0RLcd6n1cMLwRE.zXnIaScA2FXGeTJ4v0f11pawRfv0iOJZUEFi68DdLK.vWoppK06qwnJc__uPbPGgFS2qKlFlXJvy_9hU7FzpsNOC6QE288Z7.FscL_qPV.sQWW98cvqXexTy1zvITejs36KPNQgjbaGbvrdNMj9BpRRab9ZhYX.3.v.awMw1fLJa4aJemQhJEndlBphXQXK0xizkKCChVuZTvJgJgmLbkt_DI.vpeM4Nw3M2TUtFlO0Y1yK0"},
-        {"name": "__gads", "value": "ID=085f3a3b20b61c08:T=1733445963:RT=1733454458:S=ALNI_MZn46y65AO24-UxUXuczosj6zjQEA"},
-        {"name": "__gpi", "value": "UID=00000f84df21816b:T=1733445963:RT=1733454458:S=ALNI_MYRvs6JoOYQmQ0tScujaSKfHUQnPQ"},
-        {"name": "__eoi", "value": "ID=d0041aa71a542439:T=1733445963:RT=1733454458:S=AA-Afjb1epeoHDWBYuiRpjpL7XKN"},
-        {"name": "_ga_N9HWH8N387", "value": "GS1.1.1733454454.2.1.1733454593.0.0.0"},
+        {"name": "__gads", "value": "ID=085f3a3b20b61c08:T=1733445963:RT=1733558620:S=ALNI_MZn46y65AO24-UxUXuczosj6zjQEA"},
+        {"name": "__gpi", "value": "UID=00000f84df21816b:T=1733445963:RT=1733558620:S=ALNI_MYRvs6JoOYQmQ0tScujaSKfHUQnPQ"},
+        {"name": "__eoi", "value": "ID=d0041aa71a542439:T=1733445963:RT=1733558620:S=AA-Afjb1epeoHDWBYuiRpjpL7XKN"},
+        {"name": "_ga_F1VRNQ2LMP", "value": "GS1.1.1733557869.6.1.1733558618.0.0.0"},
     ]
 
 
@@ -71,7 +71,7 @@ def open_to_website(browser):
     results = []
 
 
-    browser.get("https://fairfieldctmoms.com/resources/activities-and-classes/")
+    browser.get("https://fairfieldctmoms.com/resources/child-care/")
     time.sleep(10)
 
 
@@ -81,7 +81,7 @@ def open_to_website(browser):
     soup = BeautifulSoup(page_source, "html.parser")
 
 
-    output_ = 'Activities & Classes'
+    output_ = 'Childcare'
 
     divs = soup.find_all('div', class_='et_pb_tab_content')
 
