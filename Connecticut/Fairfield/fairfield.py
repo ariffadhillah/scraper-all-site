045ebdfebe9af6961aa2518c23031845a5106e5d
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-12-07 21:15:45 +0700

    scrape site fairfieldctmoms.com country Connecticut county Fairfield section Fitness

--- Connecticut/Fairfield/fairfield.py
+++ Connecticut/Fairfield/fairfield.py
@@ -71,7 +71,7 @@ def open_to_website(browser):
     results = []
 
 
-    browser.get("https://fairfieldctmoms.com/resources/beauty/")
+    browser.get("https://fairfieldctmoms.com/resources/fitness/")
     time.sleep(10)
 
 
@@ -82,7 +82,7 @@ def open_to_website(browser):
 
     results = []
 
-    title_name = 'Beauty'
+    title_name = 'Fitness'
     
     # reviews_1 = soup.find_all('div', class_=['et_pb_tab et_pb_tab_0 clearfix', 'et_pb_tab et_pb_tab_1 clearfix', 'et_pb_tab et_pb_tab_2 clearfix','et_pb_tab et_pb_tab_3 clearfix','et_pb_tab et_pb_tab_4 clearfix','et_pb_tab et_pb_tab_5 clearfix','et_pb_tab et_pb_tab_6 clearfix','et_pb_tab et_pb_tab_7 clearfix','et_pb_tab et_pb_tab_8 clearfix','et_pb_tab et_pb_tab_9 clearfix','et_pb_tab et_pb_tab_10 clearfix','et_pb_tab et_pb_tab_11 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_13 clearfix','et_pb_tab et_pb_tab_14 clearfix','et_pb_tab et_pb_tab_15 clearfix','et_pb_tab et_pb_tab_16 clearfix','et_pb_tab et_pb_tab_17 clearfix','et_pb_tab et_pb_tab_18 clearfix','et_pb_tab et_pb_tab_19 clearfix','et_pb_tab et_pb_tab_20 clearfix','et_pb_tab et_pb_tab_21 clearfix'])
 
