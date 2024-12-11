dede2f1627641c56ae356a43b76f503516973634
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-12-11 22:27:32 +0700

    scrape site newcanaandarienmoms.com country Connecticut county New Canaan & Darien section PERSONAL GROWTH & FUN FOR MOMS

--- Connecticut/New Canaan & Darien/new-canaan-darien.py
+++ Connecticut/New Canaan & Darien/new-canaan-darien.py
@@ -71,7 +71,7 @@ def open_to_website(browser):
     results = []
 
 
-    browser.get("https://newcanaandarienmoms.com/resources/new-moms/")
+    browser.get("https://newcanaandarienmoms.com/resources/personal-growth/")
     time.sleep(10)
 
 
@@ -82,7 +82,7 @@ def open_to_website(browser):
 
     results = []
 
-    title_name = 'NEW MOMS'
+    title_name = 'PERSONAL GROWTH & FUN FOR MOMS'
     
     # reviews_1 = soup.find_all('div', class_=['et_pb_tab et_pb_tab_0 clearfix', 'et_pb_tab et_pb_tab_1 clearfix', 'et_pb_tab et_pb_tab_2 clearfix','et_pb_tab et_pb_tab_3 clearfix','et_pb_tab et_pb_tab_4 clearfix','et_pb_tab et_pb_tab_5 clearfix','et_pb_tab et_pb_tab_6 clearfix','et_pb_tab et_pb_tab_7 clearfix','et_pb_tab et_pb_tab_8 clearfix','et_pb_tab et_pb_tab_9 clearfix','et_pb_tab et_pb_tab_10 clearfix','et_pb_tab et_pb_tab_11 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_13 clearfix','et_pb_tab et_pb_tab_14 clearfix','et_pb_tab et_pb_tab_15 clearfix','et_pb_tab et_pb_tab_16 clearfix','et_pb_tab et_pb_tab_17 clearfix','et_pb_tab et_pb_tab_18 clearfix','et_pb_tab et_pb_tab_19 clearfix','et_pb_tab et_pb_tab_20 clearfix','et_pb_tab et_pb_tab_21 clearfix'])
 
@@ -91,7 +91,7 @@ def open_to_website(browser):
     # reviews_1 = soup.find_all('div', class_=['et_pb_tab et_pb_tab_0 clearfix', 'et_pb_tab et_pb_tab_1 clearfix', 'et_pb_tab et_pb_tab_2 clearfix','et_pb_tab et_pb_tab_3 clearfix','et_pb_tab et_pb_tab_4 clearfix'])
 
     for page_ in reviews_1:
-        paragraphs_1 = page_.find_all('h4')
+        paragraphs_1 = page_.find_all('p')
         # print(paragraphs_1)
 
         for p in paragraphs_1:
