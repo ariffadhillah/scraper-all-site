433fc04b2eed0687ecb238544c561444366d56e6
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-12-12 22:58:44 +0700

    scrape site newcanaandarienmoms.com country Connecticut county New Canaan & Darien section Fitness Health Wellness

--- Connecticut/New Canaan & Darien/new-canaan-darien.py
+++ Connecticut/New Canaan & Darien/new-canaan-darien.py
@@ -71,7 +71,7 @@ def open_to_website(browser):
     results = []
 
 
-    browser.get("https://newcanaandarienmoms.com/resources/personal-growth/")
+    browser.get("https://newcanaandarienmoms.com/resources/fitness-health-wellness/")
     time.sleep(10)
 
 
@@ -82,7 +82,7 @@ def open_to_website(browser):
 
     results = []
 
-    title_name = 'PERSONAL GROWTH & FUN FOR MOMS'
+    title_name = 'Fitness Health Wellness'
     
     # reviews_1 = soup.find_all('div', class_=['et_pb_tab et_pb_tab_0 clearfix', 'et_pb_tab et_pb_tab_1 clearfix', 'et_pb_tab et_pb_tab_2 clearfix','et_pb_tab et_pb_tab_3 clearfix','et_pb_tab et_pb_tab_4 clearfix','et_pb_tab et_pb_tab_5 clearfix','et_pb_tab et_pb_tab_6 clearfix','et_pb_tab et_pb_tab_7 clearfix','et_pb_tab et_pb_tab_8 clearfix','et_pb_tab et_pb_tab_9 clearfix','et_pb_tab et_pb_tab_10 clearfix','et_pb_tab et_pb_tab_11 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_13 clearfix','et_pb_tab et_pb_tab_14 clearfix','et_pb_tab et_pb_tab_15 clearfix','et_pb_tab et_pb_tab_16 clearfix','et_pb_tab et_pb_tab_17 clearfix','et_pb_tab et_pb_tab_18 clearfix','et_pb_tab et_pb_tab_19 clearfix','et_pb_tab et_pb_tab_20 clearfix','et_pb_tab et_pb_tab_21 clearfix'])
 
