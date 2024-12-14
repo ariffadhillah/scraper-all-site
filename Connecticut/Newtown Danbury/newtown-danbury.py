7a14ecb94f513ec6848741ff6d1427ceb4ba77f6
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2024-12-14 08:06:50 +0700

    scrape site newtownmoms.com country Connecticut county Newtown Danbury  section New Moms

--- Connecticut/Newtown Danbury/newtown-danbury.py
+++ Connecticut/Newtown Danbury/newtown-danbury.py
@@ -71,7 +71,7 @@ def open_to_website(browser):
     results = []
 
 
-    browser.get("https://newtownmoms.com/resources/mom-life")
+    browser.get("https://newtownmoms.com/resources/new-moms/")
     time.sleep(10)
 
 
@@ -82,11 +82,11 @@ def open_to_website(browser):
 
     results = []
 
-    title_name = 'Mom Life'
+    title_name = 'New Moms'
     
     # reviews_1 = soup.find_all('div', class_=['et_pb_tab et_pb_tab_0 clearfix', 'et_pb_tab et_pb_tab_1 clearfix', 'et_pb_tab et_pb_tab_2 clearfix','et_pb_tab et_pb_tab_3 clearfix','et_pb_tab et_pb_tab_4 clearfix','et_pb_tab et_pb_tab_5 clearfix','et_pb_tab et_pb_tab_6 clearfix','et_pb_tab et_pb_tab_7 clearfix','et_pb_tab et_pb_tab_8 clearfix','et_pb_tab et_pb_tab_9 clearfix','et_pb_tab et_pb_tab_10 clearfix','et_pb_tab et_pb_tab_11 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_12 clearfix','et_pb_tab et_pb_tab_13 clearfix','et_pb_tab et_pb_tab_14 clearfix','et_pb_tab et_pb_tab_15 clearfix','et_pb_tab et_pb_tab_16 clearfix','et_pb_tab et_pb_tab_17 clearfix','et_pb_tab et_pb_tab_18 clearfix','et_pb_tab et_pb_tab_19 clearfix','et_pb_tab et_pb_tab_20 clearfix','et_pb_tab et_pb_tab_21 clearfix'])
 
-    reviews_1 = soup.find_all('div', class_=['et_pb_tab et_pb_tab_0 clearfix', 'et_pb_tab et_pb_tab_1 clearfix', 'et_pb_tab et_pb_tab_2 clearfix','et_pb_tab et_pb_tab_3 clearfix','et_pb_tab et_pb_tab_4 clearfix','et_pb_tab et_pb_tab_5 clearfix'])
+    reviews_1 = soup.find_all('div', class_=['et_pb_tab et_pb_tab_0 clearfix', 'et_pb_tab et_pb_tab_1 clearfix', 'et_pb_tab et_pb_tab_2 clearfix','et_pb_tab et_pb_tab_3 clearfix','et_pb_tab et_pb_tab_4 clearfix','et_pb_tab et_pb_tab_5 clearfix','et_pb_tab et_pb_tab_6 clearfix','et_pb_tab et_pb_tab_7 clearfix','et_pb_tab et_pb_tab_8 clearfix','et_pb_tab et_pb_tab_9 clearfix','et_pb_tab et_pb_tab_10 clearfix','et_pb_tab et_pb_tab_11 clearfix'])
 
     # reviews_1 = soup.find_all('div', class_=['et_pb_toggle_content clearfix'])
     
